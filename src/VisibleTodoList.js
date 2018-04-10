d0aeb007dfcd852c9337852abf41c3e3b23fe822
unknown <thekajalsharma007@gmail.com>
2018-04-10 17:16:50 +0530

    Refacored VisibleTodoList class, extracted TodoList component and removed getVisibleTodoArray method from the component.

--- src/VisibleTodoList.js
+++ src/VisibleTodoList.js
@@ -2,8 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Todo from './Todo';
 
-class VisibleTodoList extends React.Component {
+const getVisibleTodoArray = (todoArray, visibilityFilter) => {
+    switch (visibilityFilter) {
+        case 'SHOW_ALL':
+            return todoArray;
+        case 'SHOW_COMPLETED':
+            return todoArray.filter((todo) => todo.completed);
+        case 'SHOW_ACTIVE':
+            return todoArray.filter((todo) => !todo.completed);
+    }
+}
 
+class VisibleTodoList extends React.Component {
     componentDidMount() {
         const { store } = this.context;
         this.unsubscribe = store.subscribe(() =>
@@ -15,53 +25,52 @@ class VisibleTodoList extends React.Component {
         this.unsubscribe();
     }
 
-    getVisibleTodoArray = (todoArray, visibilityFilter) => {
-        //  return todoArray;
-        console.log("**************");
-        const { store } = this.context;
-        switch (visibilityFilter) {
-            case 'SHOW_ALL':
-                console.log("In Show all ");
-                console.log(store.getState());
-                return todoArray;
-            case 'SHOW_COMPLETED':
-                console.log("In Show completed ");
-                console.log(store.getState());
-                const visibleArray = todoArray.filter((todo) => todo.completed);
-                console.log("Visible Array");
-                console.log(visibleArray);
-                return visibleArray;
-            case 'SHOW_ACTIVE':
-                console.log("In Show active");
-                return todoArray.filter((todo) => !todo.completed);
-        }
-        // const visibleTodoArray = [{ id: 0, text: "One", completed: false }];
-        // return visibleTodoArray;
-    }
-
     render() {
         const { store } = this.context;
-        const todoList = this.getVisibleTodoArray(store.getState().todoArray, store.getState().visibilityFilter);
-        console.log("TODOList");
-        console.log(todoList);
+
         return (
-            <ul>
-                {todoList.map((todo) =>
-                    <Todo
-                        key={todo.id}
-                        onClick={() => store.dispatch({
-                            type: 'TOGGLE_TO_DO',
-                            id: todo.id
-                        })}
-                        completed={todo.completed}
-                        text={todo.text} />
-                )}
-            </ul>
+            <TodoList
+                todoList={getVisibleTodoArray(store.getState().todoArray, store.getState().visibilityFilter)}
+                onTodoClick={(id) => store.dispatch({
+                    type: 'TOGGLE_TO_DO',
+                    id
+                })} />
         );
     }
+    mapDispatchToProps = (dispatch) => {
+        return {
+            onClick: (id) => {
+                dispatch({
+                    type: 'TOGGLE_TODO',
+                    id
+                })
+            }
+        };
+    };
 
+    mapStateToProps = (state) => {
+        return {
+            todoList: getVisibleTodoArray(
+                state.todoArray,
+                state.visibilityFilter
+            )
+        };
+    }
 }
 VisibleTodoList.contextTypes = {
     store: PropTypes.object
 }
 export default VisibleTodoList;
+
+const TodoList = ({ todoList, onTodoClick }) => {
+    return (
+        <ul>
+            {todoList.map((todo) =>
+                <Todo
+                    key={todo.id}
+                    onClick={() => onTodoClick(todo.id)}
+                    completed={todo.completed}
+                    text={todo.text} />
+            )}
+        </ul>);
+}
